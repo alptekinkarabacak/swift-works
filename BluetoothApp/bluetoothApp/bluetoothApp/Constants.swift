7ee5b3ecf555af6923a971e0706afcb9d2436a1a
Alptekin Karabacak <alptekinkarabacak@gmail.com>
2020-11-29 00:50:56 +0300

    Testing phase

---
+++ BluetoothApp/bluetoothApp/bluetoothApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  bluetoothApp
+//
+//  Created by Alptekin Karabacak on 28.10.2020.
+//
+
+enum PeripheralNotificationKeys : String { // The notification name of peripheral
+    case DisconnectNotif = "disconnectNotif" // Disconnect notification name
+    case CharacteristicNotif = "characteristicNotif" // Characteristic discover notification name
+}
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var str = ["a", "b", "c", "d", 1] as Any
+
+var dict = ["birinci" : "ye", "ikinci" : 1] as [String : Any]
+    
+
+
