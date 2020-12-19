8aa0ad19332cfd926e1231000d7cb0066188c33f
Alptekin Karabacak <alptekinkarabacak@gmail.com>
2020-12-19 20:11:31 +0300

    End Game

--- ArtBookProject/ArtBookProject/DetailsVC.swift
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -13,8 +14,70 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if chosenPainting != "" {
+            
+            // Disable
+            saveButton.isHidden = true
+            // saveButton.isHidden = true -> to make invisible
+            // Get data from core data
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingId?.uuidString
+            // Search datas get from Core Data, filtering process
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                // Returns
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                    
+                }
+                
+            } catch {
+                print("An Error has Occured!")
+            }
+            
+            
+            
+            
+            
+        } else {
+            // Default values creating another entry from Core Data
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            nameText.text = ""
+            artistText.text = ""
+            yearText.text = ""
+        }
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
@@ -25,7 +88,35 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         imageView.addGestureRecognizer(imageTapforSave)
     }
     @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(artistText.text!, forKey: "artist")
+        if let year = Int(yearText.text!) {
+            newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("Succes")
+        } catch {
+            print("Error")
+        }
+        
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        saveButton.isEnabled = true
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func hideKeyboard() {
@@ -35,6 +126,10 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @objc func saveSelector() {
         let picker = UIImagePickerController()
         picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
         
     }
     
--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -6,18 +6,118 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    var selectedPainting = ""
+    var selectedPaintingId: UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        getData()
         
     }
     
-    @objc func addButtonClicked() {
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // Fetching means, getting informations from database.
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]  {
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
+                
+            }
+        } catch {
+            print("Error")
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVc" {
+            let destinationVc = segue.destination as! DetailsVC
+            destinationVc.chosenPainting = selectedPainting
+            destinationVc.chosenPaintingId = selectedPaintingId
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingId = idArray[indexPath.row]
         performSegue(withIdentifier: "toDetailsVc", sender: nil)
+    }
+    
+    @objc func addButtonClicked() {
+        selectedPainting = ""
+        performSegue(withIdentifier: "toDetailsVc", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // To delete an tableView item we need to get the data will be deleted from Core data.
+        let appDelegate  = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let idString = idArray[indexPath.row].uuidString
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let id = result.value(forKey: "id") as? UUID {
+                        if id == idArray[indexPath.row] {
+                            // Delete Data from Core Data
+                            context.delete(result)
+                            nameArray.remove(at: indexPath.row)
+                            idArray.remove(at: indexPath.row)
+                            self.tableView.reloadData()
+                            
+                            do {
+                                try context.save()
+                            } catch {
+                                print("An Error has OCcured!")
+                            }
+                            break
+                        }
+                    }
+                }
+            }
+        } catch  {
+            print("An Error has Occured!")
+        }
+        
+        
     }
 
 
